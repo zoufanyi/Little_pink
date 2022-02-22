6642edcb67b60313bcfe916ddc82f49cbd2c1354
fzou0001 <fzou0001@student.monash.edu>
2022-02-22 15:54:25 +0800

    Initial Commit

---
+++ LittlePinkUITests/LittlePinkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LittlePinkUITestsLaunchTests.swift
+//  LittlePinkUITests
+//
+//  Created by Mr. Zou on 2022/2/22.
+//
+
+import XCTest
+
+class LittlePinkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
